4271f3f05a27ee7a71736f7bbc4c4711406e3b01
unknown <COLLABERA.DIGITAL@IBPHNB1001159.collaberadigital.tech>
2023-10-21 11:24:27 +0800

    [ADD]: added base package to component scan annotation

--- src/main/java/com/example/sampleproj/SampleprojApplication.java
+++ src/main/java/com/example/sampleproj/SampleprojApplication.java
@@ -3,17 +3,13 @@ package com.example.sampleproj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-
-@RequestMapping(value ="/")
-@ComponentScan(basePackages = "com.example.sampleproj")
+@ComponentScan(basePackages = "com.example.sampleproj, com.example.welcome_page")
 @SpringBootApplication
 public class SampleprojApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleprojApplication.class, args);
-		System.out.println("SampleProjApplication WORKED?");
 	}
 
 }
