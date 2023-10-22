20fdd560e82b8aa616bb9df3b490571e2e6669e2
unknown <COLLABERA.DIGITAL@IBPHNB1001159.collaberadigital.tech>
2023-10-22 22:10:21 +0800

    [MOD]: modified changes for application starter

--- src/main/java/com/example/starter/MainApplication.java
+++ src/main/java/com/example/starter/MainApplication.java
@@ -1,15 +1,15 @@
-package com.example.sampleproj;
+package com.example.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.example.sampleproj, com.example.welcome_page")
+@ComponentScan(basePackages = "com.example.starter, com.example.welcome_page, com.example.account, com.example.messages")
 @SpringBootApplication
-public class SampleprojApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleprojApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
